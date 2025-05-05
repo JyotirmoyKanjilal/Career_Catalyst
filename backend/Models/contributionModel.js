1c6b88dcd2a8d7c0d9da97b17d3e5285792015f1
m-triple-m <triplem656@gmail.com>
2025-05-05 11:08:36 +0530

    fixed contributions

--- backend/Models/contributionModel.js
+++ backend/Models/contributionModel.js
@@ -2,7 +2,6 @@ const { Schema, model } = require('../connection');
 
 const contributionSchema = new Schema({
   questionId: { type: Schema.Types.ObjectId, ref: 'questions', required: true }, // Reference to the question
-  question: { type: String, required: true }, // Question text
   answer: { type: String, required: true }, // User's answer
   timestamp: { type: Date, default: Date.now }, // Timestamp of the contribution
   status: { type: String, enum: ['Pending', 'Approved', 'Rejected'], default: 'Pending' }, // Status of the contribution
